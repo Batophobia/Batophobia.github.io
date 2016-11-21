4f99c87faa3ab725a7fdcf5c10201066b63c0152
Batophobia <godrox3@yahoo.com>
2016-11-21 10:54:28 -0600

    Update player.js

--- test/player.js
+++ test/player.js
@@ -12,6 +12,7 @@ function onYouTubeIframeAPIReady() {
       key: apiKey
     }, function(retData){
       var listLength = retData.pageInfo.totalResults;
+      var nextPage = retData.nextPageToken;
       for(i=0;i<retData.items.length;i++){
         videoIDs.push(retData.items[i].snippet.resourceId.videoId);
       }
