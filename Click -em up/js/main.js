bee68934a57f6f2dccc84723ca2258a9ac1bdaba
Batophobia <godrox3@yahoo.com>
2017-03-24 13:17:10 -0500

    Create main.js

---
+++ Click -em up/js/main.js
@@ -0,0 +1,54 @@
+var main = {
+	init: function(){
+		this.load();
+		ments.init();
+		display.init();
+	},
+	timerID: 0,
+	counter: 0,
+	delay: 20,
+	
+	tick : function(){
+		ments.tick();
+		
+		this.save();
+		this.counter++;
+	},
+	
+	save : function(){
+		var data = {'map':[],'ments':{}};
+		
+		for(var itm in ments.list)
+			data['ments'][itm] = ments.list[itm].unlk;
+		
+		data['ments'].stats = ments.stats;
+		
+		//localStorage["1_x"] = JSON.stringify(data);
+	},
+	
+	load : function(){
+		if('1_x' in localStorage){
+			var data = JSON.parse(localStorage['1_x']);
+		}else{
+			return;
+		}
+	},
+	
+	alrt: function(input){
+		var alert = $("<div class='alert'>" + input + "</div>");
+		$('#alerts').append(alert);
+		setTimeout(function(){
+			alert.fadeOut('slow',function(){
+				$(this).remove();
+			});
+		},3000);
+	},
+};
+
+Object.size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
