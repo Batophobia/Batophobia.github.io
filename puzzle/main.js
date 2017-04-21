4eda4b07f6f8ed9d46cda314fd322049b3deacba
Batophobia <godrox3@yahoo.com>
2017-04-21 15:13:44 -0500

    Update main.js

--- puzzle/main.js
+++ puzzle/main.js
@@ -8,7 +8,7 @@ var main = {
     riverB: { name: "Dark River", dir: { north: "riverT", south: "bridge", west: "town" }, text: "The breathtaking view of a wide river greets you, the sunlight sparkling off the water.  Some birds are singing nearby as you watch the fish swimming in the deep waters." },
     bridge: { name: "Highland Bridge", dir: { north: "riverB", east: "plains" }, text: "You walk up to a bridge that crosses the river.  As you begin to cross the bridge, a troll pops out.  \"19 55 73 91 121\", it says.  \"What are you talking about?\" you ask. \"19 55 73 91 121\", it repeats." },
     plains: { name: "Expansive Plain", dir: { north: "forestBL", west: "bridge", }, text: "BV NRQIMZOGU QA G QPBGGUF QABWUMF ECV QA. \"RW JA VT HIM GUD MMSZ, P UWC XWHPG, IZ CWGZCN TRLH, BVQ Q WO BUK AJLQRS.  CCG NCX ERXS BTY ZVF ZRYH JV GNSSM!\"  UK'G QWVTHJVT GH B 3 JL 3 MFJL, JOGQMEOBH BB NWNARRT \"UPR QSZ QF IZPKXCWTM...\"" },
-    forestTL: { name: "Red Forest", dir: {  }, text: "Forest 1" },
+    forestTL: { name: "Red Forest", dir: { south: "plains" }, text: "Forest 1" },
     forestTR: { name: "Rounded Wood", dir: {  }, text: "Forest 2" },
     forestBL: { name: "Inner Forest", dir: {  }, text: "Forest 3" },
     forestBR: { name: "Nice Cabin", dir: {  }, text: "Forest 4" },
@@ -34,3 +34,7 @@ var main = {
     this.loadLocation();
   }
 };
+
+$(document).on("click","#dirButtons button", function(e){
+	main.move($(this).attr("dir"));
+})
