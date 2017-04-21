755c4fbb579277c7e08ca155a764e582fc2f4853
Batophobia <godrox3@yahoo.com>
2017-04-21 15:06:14 -0500

    Update main.js

--- puzzle/main.js
+++ puzzle/main.js
@@ -4,14 +4,14 @@ var main = {
   },
   locations: {
     town: { name: "Ruklo Town", dir: { east: "riverB" }, text: "This is the town you started in.  There's nothing left to help you on our journey here." },
-    riverT: { name: "Eddied Stream", dir: { south: "riverB" }, text: "River 1" },
-    riverB: { name: "Dark River", dir: { north: "riverT", south: "bridge", west: "town" }, text: "River 2" },
-    bridge: { name: "Highland Bridge", dir: { north: "riverB", east: "plains" }, text: "Bridge" },
-    plains: { name: "Expansive Plain", dir: { north: "forestBL", west: "bridge", }, text: "Plains" },
+    riverT: { name: "Eddied Stream", dir: { south: "riverB" }, text: "As you walk up to the babbling waters of the river, you notice a bear in the distance.  Seemingly, it's decided to neglect your intrusion.  You probably could turn around without causing an issue." },
+    riverB: { name: "Dark River", dir: { north: "riverT", south: "bridge", west: "town" }, text: "The breathtaking view of a wide river greets you, the sunlight sparkling off the water.  Some birds are singing nearby as you watch the fish swimming in the deep waters." },
+    bridge: { name: "Highland Bridge", dir: { north: "riverB", east: "plains" }, text: "You walk up to a bridge that crosses the river.  As you begin to cross the bridge, a troll pops out.  \"19 55 73 91 121\", it says.  \"What are you talking about?\" you ask. \"19 55 73 91 121\", it repeats." },
+    plains: { name: "Expansive Plain", dir: { north: "forestBL", west: "bridge", }, text: "BV NRQIMZOGU QA G QPBGGUF QABWUMF ECV QA. \"RW JA VT HIM GUD MMSZ, P UWC XWHPG, IZ CWGZCN TRLH, BVQ Q WO BUK AJLQRS.  CCG NCX ERXS BTY ZVF ZRYH JV GNSSM!\"  UK'G QWVTHJVT GH B 3 JL 3 MFJL, JOGQMEOBH BB NWNARRT \"UPR QSZ QF IZPKXCWTM...\"" },
     forestTL: { name: "Red Forest", dir: {  }, text: "Forest 1" },
     forestTR: { name: "Rounded Wood", dir: {  }, text: "Forest 2" },
     forestBL: { name: "Inner Forest", dir: {  }, text: "Forest 3" },
-    forestBR: { name: "Nice Cottage", dir: {  }, text: "Forest 4" },
+    forestBR: { name: "Nice Cabin", dir: {  }, text: "Forest 4" },
     mountain: { name: "Grencidep Mountain", dir: {  }, text: "Mountain" },
   },
   current: "town",
@@ -23,9 +23,14 @@ var main = {
     var key = $("#txeKey").val();
     $("#locName").text(this.locations[this.current].name);
     $("#locText").text( vigenere(this.locations[this.current].text, key) );
+    
+    $("#dirButtons buttons").hide();
+    for(var direction in this.locations[this.current].dir){
+      $("#"+direction+"Btn").show();
+    }
   },
-  move: function(locName){
-    this.current = locName;
+  move: function(direction){
+    this.current = this.locations[this.current].dir[direction];
     this.loadLocation();
   }
 };
