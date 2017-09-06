3b80cb736cba8679fa387405d247407b27b5b74d
Batophobia <godrox3@yahoo.com>
2017-09-05 20:29:59 -0500

    Update main.js

--- shows/main.js
+++ shows/main.js
@@ -7,18 +7,21 @@ $(function(){
     searchShow();
   });
   
-  $("#navBar li").click(function(){
+  $("#navBar li").click(function(e){
     $(".section").hide();
     $("#"+$(this).attr("for")).show();
   });
   
-  $(document).on("click", ".toggleShow", function(){
+  $(document).on("click", ".toggleShow", function(e){
     if(UserShows[$(this).parent().attr("for")]){
-      UserShows[$(this).parent().attr("for")] = true;
-      msg("Added Show");
-    }else{
       delete UserShows[$(this).parent().attr("for")];
       msg("Removed Show");
+    }else{
+      UserShows[$(this).parent().attr("for")] = {
+        name: $(this).parent().find("showName"),
+        image: $(this).parent().find("showImage")
+      };
+      msg("Added Show");
     }
     saveShows();
   });
@@ -36,7 +39,7 @@ function searchShow(){
         "<tr for='"+retData[show].show.id+"'>"
         +"<td class='toggleShow'>Add Show</td>"
         +"<td class='showName'>"+retData[show].show.name+"</td>"
-        +"<td><img src='"+retData[show].show.image.medium+"' /></td>"
+        +"<td><img class='showImage' src='"+retData[show].show.image.medium+"' /></td>"
         +"<td>"+retData[show].show.summary+"</td>"
         +"</tr>"
       );
@@ -53,6 +56,14 @@ function loadShows(){
   
   UserShows = JSON.parse( localStorage["ShowTracker"] );
   //20 calls per 10 seconds
+  
+  for(var show in UserShows){
+    $("#secShows").append(
+      "<span class='showWrapper' for='"+show+"'>"+
+      "<div class='showName'>"+UserShows[show].name+"</div>"+
+      "<img src='"+UserShows[show].image+"' />"+
+      "</span>");
+  }
 }
 
 function msg(input){
