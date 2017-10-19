a62462b7d870c3430d67c4af8ef2c856e2667362
Batophobia <godrox3@yahoo.com>
2017-10-19 09:48:55 -0500

    Update main.js

--- Bible/js/main.js
+++ Bible/js/main.js
@@ -1,13 +1,74 @@
-//authorize apps
+/******************** GLOBAL VARIABLES ********************/
+var SCOPES = ['https://www.googleapis.com/auth/drive','profile'];
+var CLIENT_ID = 'YOUR_CLIENT_ID.apps.googleusercontent.com';
+var API_KEY = 'YOUR_API_KEY';
+var FOLDER_NAME = "";
+var FOLDER_ID = "root";
+var FOLDER_PERMISSION = true;
+var FOLDER_LEVEL = 0;
+var NO_OF_FILES = 1;
+var DRIVE_FILES = [];
+var FILE_COUNTER = 0;
+var FOLDER_ARRAY = [];
+
+$(function(){
+  showUserInfo();
+});
+
+function handleClientLoad() {
+	gapi.load('client:auth2', initClient);
+}
+
 function initClient() {
 	gapi.client.init({
-		apiKey: API_KEY, //THIS IS OPTIONAL AND WE DONT ACTUALLY NEED THIS, BUT I INCLUDE THIS AS EXAMPLE
+		apiKey: API_KEY,
 		clientId: CLIENT_ID,
 		scope: SCOPES.join(' ')
 	}).then(function () {
-	  // Listen for sign-in state changes.
 	  gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-	  // Handle the initial sign-in state.
 	  updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
 	});
+}
+
+function updateSigninStatus(isSignedIn) {
+	if (isSignedIn) {
+	  showLoading();
+    getDriveFiles();
+	} else {
+		// Need to log in
+	}
+}
+
+function handleAuthClick(event) {
+	gapi.auth2.getAuthInstance().signIn();
+}
+
+function handleSignoutClick(event) {
+	if(confirm("Are you sure you want to logout?")){
+		gapi.auth2.getAuthInstance().signOut();
+	}
+}
+
+function showUserInfo(){
+	var request = gapi.client.drive.about.get();
+    var obj = {};
+    request.execute(function(resp) { 
+       if (!resp.error) {
+			$("#passageTitle").html(resp.name);
+			$("#passageText").html(formatBytes(resp.quotaBytesTotal));
+			$("#passageTags").html(formatBytes(resp.quotaBytesUsed));
+       }else{
+            showMessage("Error",resp.error.message);
+       }
+   });
+}
+
+function showMessage(type, message){
+  var alert = $("<div class='alert "+type+"'>" + message + "</div>");
+	$('#messages').append(alert);
+	setTimeout(function(){
+		alert.fadeOut('slow',function(){
+			$(this).remove();
+		});
+  },3000);
 }
