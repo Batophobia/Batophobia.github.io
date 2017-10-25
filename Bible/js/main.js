d9815e86018bd76b64fbca6428c34c96bfeadaac
Batophobia <godrox3@yahoo.com>
2017-10-25 09:26:07 -0500

    Update main.js

--- Bible/js/main.js
+++ Bible/js/main.js
@@ -1,12 +1,21 @@
 var apikey = "472e4906ffa57fc8e08a58b0a8bf46eb";
 
 $(function(){
-	$.get("https://api.biblia.com/v1/bible/find",{key: apikey})
+	getBibles();
+});
+
+function getBibles(){
+  $.get("https://api.biblia.com/v1/bible/find",{key: apikey})
   .done(function(data){
     $("#passageText").text("");
-    data = JSON.parse(data);
-    for(var itm in data){
-      $("#passageText").append(itm+": "+data[itm]+"<br/>");
+    //data = JSON.parse(data);
+    for(var itm in data.bibles){
+      var optBible = "<div class='BibleOption'>";
+      for(var elem in data.bibles[itm]){
+        optBible = optBible + "<div>"+elem+": "+data.bibles[itm][elem]+"</div>";
+      }
+      optBible = optBible + "</div>";
+      $("#passageText").append(optBible);
     }
   });
-});
+}
