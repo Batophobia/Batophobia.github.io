a681e220410259d282af2559c47839a098ab5a0e
Batophobia <godrox3@yahoo.com>
2017-10-25 09:19:26 -0500

    Update main.js

--- Bible/js/main.js
+++ Bible/js/main.js
@@ -4,7 +4,7 @@ $(function(){
 	$.get("https://api.biblia.com/v1/bible/find",{key: apikey})
   .done(function(data){
     $("#passageText").text("");
-    data = Json.parse(data);
+    data = JSON.parse(data);
     for(var itm in data){
       $("#passageText").append(itm+": "+data[itm]+"<br/>");
     }
