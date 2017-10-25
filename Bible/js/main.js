23704fc233d5cc34856973ec3d7decff5a67834b
Batophobia <godrox3@yahoo.com>
2017-10-25 09:37:40 -0500

    Change to  biblegateway from biblia api
    http://bibliaapi.com/docs/Bible_Services

--- Bible/js/main.js
+++ Bible/js/main.js
@@ -1,21 +1,10 @@
-var apikey = "472e4906ffa57fc8e08a58b0a8bf46eb";
-
 $(function(){
 	getBibles();
 });
 
 function getBibles(){
-  $.get("https://api.biblia.com/v1/bible/find",{key: apikey})
+  $.get("http://www.biblegateway.com/passage",{search: "Genesis 1", version: "NASB"})
   .done(function(data){
-    $("#passageText").text("");
-    //data = JSON.parse(data);
-    for(var itm in data.bibles){
-      var optBible = "<div class='BibleOption'>";
-      for(var elem in data.bibles[itm]){
-        optBible = optBible + "<div>"+elem+": "+data.bibles[itm][elem]+"</div>";
-      }
-      optBible = optBible + "</div>";
-      $("#passageText").append(optBible);
-    }
+    $("#passageText").html(data);
   });
 }
