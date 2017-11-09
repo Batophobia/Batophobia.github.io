7933c8bcddefaeb3b778addb80897a9c3a5a8460
Batophobia <godrox3@yahoo.com>
2017-11-09 11:05:30 -0600

    Update game.js

--- PNCRN/js/game.js
+++ PNCRN/js/game.js
@@ -1,7 +1,17 @@
 $(function(){
-  // Draw res card from field
   $(document).on("click",".cardList td.fieldCard",function(){
+    // Draw res card from field
     var fieldNo = $(this).index();
     player.drawRes(fieldNo);
+  });
+  
+  $("#btnPoo").click(function(){
+    // Make poo
+    $("#everything").append('<div draggable="true" class="poo"></div>');
+  });
+  
+  $("#btnWork").click(function(){
+    // Make worker
+    $("#everything").append('<div draggable="true" class="worker"></div>');
   });
 });
