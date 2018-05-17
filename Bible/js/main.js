7e34b594193a9fea9191fcf2cfd62d4f6d709071
Batophobia <godrox3@yahoo.com>
2018-05-17 10:53:26 -0500

    Add OAuth2

--- Bible/js/main.js
+++ Bible/js/main.js
@@ -2,6 +2,12 @@ $(function(){
 	getBibles();
 });
 
+function onSignIn(user) {
+  var profile = user.getBasicProfile();
+  $('#profile .name').text(profile.getName());
+  $('#profile .email').text(profile.getEmail());
+}
+
 function getBibles(){
   $.get("https://www.biblegateway.com/passage",{search: "Genesis 1", version: "NASB"})
   .done(function(data){
