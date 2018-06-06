e9c0a319588cc1362dace2702c2d8aa82c726a11
Batophobia <godrox3@yahoo.com>
2018-06-05 23:07:33 -0500

    Create test.js
    Can paste this into dev console to test random

---
+++ Random/test.js
@@ -0,0 +1,16 @@
+$("#txtPeople").val("David Starkey\nChristina Starkey\nChris Farhner\nDiane Starkey\nValca Castillo");
+$("#btnUpdate").click();
+$(".numEntry[for='Christina Starkey']").val(50);
+$(".numEntry[for='Valca Castillo']").val(25);
+
+var ti=0;
+
+function doWin(){
+  $("#btnPick").click();
+  setTimeout(function(){
+		$("#closeWin").click();
+    doWin();
+	},500);
+}
+
+doWin();
