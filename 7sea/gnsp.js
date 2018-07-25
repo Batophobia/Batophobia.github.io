ee18bc6e393326ba8300f1c3180d45e220ef325e
Batophobia <godrox3@yahoo.com>
2018-07-24 20:20:24 -0500

    Update gnsp.js

--- 7sea/gnsp.js
+++ 7sea/gnsp.js
@@ -3,7 +3,345 @@ var gnsp = {  // Adventure letters
   npc: {
     
   },
-  place:{
+  place: {
     
+  },
+  hero: {
+    nico: {
+      name: "Nico De Luca",
+      gender: "Male",
+      nation: "Vodacce",
+      virtue: "",
+      hubris: "",
+      trait: {
+        brawn: 2,
+        finesse: 3,
+        resolve: 2,
+        wits: 2,
+        panache: 4
+      },
+      skill: {
+        athletics: 2, 
+        convince: 2, 
+        empathy: 2, 
+        hide: 2, 
+        intimidate: 1, 
+        notice: 2, 
+        perform: 1, 
+        tempt: 3, 
+        theft: 2, 
+        weaponry: 3 
+      },
+      background: {
+        bravo: "Earn Hero Point when putting self in danger to defend someone sworn to protect",
+        moroso: "Earn Hero Point when choose to let another character get away with something because you're romantically interested in them"
+      },
+      advantage: {
+        1: {
+          name: "Time Sense",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        },
+        2: {
+          name: "Come Hither",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        },
+        3: {
+          name: "Poison Immunity",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        },
+        4: {
+          name: "An Honest Misunderstanding",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        },
+        5: {
+          name: "Flirting with Disaster",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        },
+        6: {
+          name: "Hard to Kill",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        }
+      }
+    },
+    rufina: {
+      name: "Nico De Luca",
+      gender: "Male",
+      nation: "Vodacce",
+      virtue: "",
+      hubris: "",
+      trait: {
+        brawn: 2,
+        finesse: 3,
+        resolve: 2,
+        wits: 2,
+        panache: 4
+      },
+      skill: {
+        athletics: 2, 
+        convince: 2, 
+        empathy: 2, 
+        hide: 2, 
+        intimidate: 1, 
+        notice: 2, 
+        perform: 1, 
+        tempt: 3, 
+        theft: 2, 
+        weaponry: 3 
+      },
+      background: {
+        bravo: "Earn Hero Point when putting self in danger to defend someone sworn to protect",
+        moroso: "Earn Hero Point when choose to let another character get away with something because you're romantically interested in them"
+      },
+      advantage: {
+        1: {
+          name: "Time Sense",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        },
+        2: {
+          name: "Come Hither",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        },
+        3: {
+          name: "Poison Immunity",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        },
+        4: {
+          name: "An Honest Misunderstanding",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        },
+        5: {
+          name: "Flirting with Disaster",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        },
+        6: {
+          name: "Hard to Kill",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        }
+      }
+    },
+    cassidy: {
+      name: "Nico De Luca",
+      gender: "Male",
+      nation: "Vodacce",
+      virtue: "",
+      hubris: "",
+      trait: {
+        brawn: 2,
+        finesse: 3,
+        resolve: 2,
+        wits: 2,
+        panache: 4
+      },
+      skill: {
+        athletics: 2, 
+        convince: 2, 
+        empathy: 2, 
+        hide: 2, 
+        intimidate: 1, 
+        notice: 2, 
+        perform: 1, 
+        tempt: 3, 
+        theft: 2, 
+        weaponry: 3 
+      },
+      background: {
+        bravo: "Earn Hero Point when putting self in danger to defend someone sworn to protect",
+        moroso: "Earn Hero Point when choose to let another character get away with something because you're romantically interested in them"
+      },
+      advantage: {
+        1: {
+          name: "Time Sense",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        },
+        2: {
+          name: "Come Hither",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        },
+        3: {
+          name: "Poison Immunity",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        },
+        4: {
+          name: "An Honest Misunderstanding",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        },
+        5: {
+          name: "Flirting with Disaster",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        },
+        6: {
+          name: "Hard to Kill",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        }
+      }
+    },
+    karahkwa: {
+      name: "Nico De Luca",
+      gender: "Male",
+      nation: "Vodacce",
+      virtue: "",
+      hubris: "",
+      trait: {
+        brawn: 2,
+        finesse: 3,
+        resolve: 2,
+        wits: 2,
+        panache: 4
+      },
+      skill: {
+        athletics: 2, 
+        convince: 2, 
+        empathy: 2, 
+        hide: 2, 
+        intimidate: 1, 
+        notice: 2, 
+        perform: 1, 
+        tempt: 3, 
+        theft: 2, 
+        weaponry: 3 
+      },
+      background: {
+        bravo: "Earn Hero Point when putting self in danger to defend someone sworn to protect",
+        moroso: "Earn Hero Point when choose to let another character get away with something because you're romantically interested in them"
+      },
+      advantage: {
+        1: {
+          name: "Time Sense",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        },
+        2: {
+          name: "Come Hither",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        },
+        3: {
+          name: "Poison Immunity",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        },
+        4: {
+          name: "An Honest Misunderstanding",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        },
+        5: {
+          name: "Flirting with Disaster",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        },
+        6: {
+          name: "Hard to Kill",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        }
+      }
+    },
+    eezkis: {
+      name: "Nico De Luca",
+      gender: "Male",
+      nation: "Vodacce",
+      virtue: "",
+      hubris: "",
+      trait: {
+        brawn: 2,
+        finesse: 3,
+        resolve: 2,
+        wits: 2,
+        panache: 4
+      },
+      skill: {
+        athletics: 2, 
+        convince: 2, 
+        empathy: 2, 
+        hide: 2, 
+        intimidate: 1, 
+        notice: 2, 
+        perform: 1, 
+        tempt: 3, 
+        theft: 2, 
+        weaponry: 3 
+      },
+      background: {
+        bravo: "Earn Hero Point when putting self in danger to defend someone sworn to protect",
+        moroso: "Earn Hero Point when choose to let another character get away with something because you're romantically interested in them"
+      },
+      advantage: {
+        1: {
+          name: "Time Sense",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        },
+        2: {
+          name: "Come Hither",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        },
+        3: {
+          name: "Poison Immunity",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        },
+        4: {
+          name: "An Honest Misunderstanding",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        },
+        5: {
+          name: "Flirting with Disaster",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        },
+        6: {
+          name: "Hard to Kill",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        }
+      }
+    },
+    jack: {
+      name: "Nico De Luca",
+      gender: "Male",
+      nation: "Vodacce",
+      virtue: "",
+      hubris: "",
+      trait: {
+        brawn: 2,
+        finesse: 3,
+        resolve: 2,
+        wits: 2,
+        panache: 4
+      },
+      skill: {
+        athletics: 2, 
+        convince: 2, 
+        empathy: 2, 
+        hide: 2, 
+        intimidate: 1, 
+        notice: 2, 
+        perform: 1, 
+        tempt: 3, 
+        theft: 2, 
+        weaponry: 3 
+      },
+      background: {
+        bravo: "Earn Hero Point when putting self in danger to defend someone sworn to protect",
+        moroso: "Earn Hero Point when choose to let another character get away with something because you're romantically interested in them"
+      },
+      advantage: {
+        1: {
+          name: "Time Sense",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        },
+        2: {
+          name: "Come Hither",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        },
+        3: {
+          name: "Poison Immunity",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        },
+        4: {
+          name: "An Honest Misunderstanding",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        },
+        5: {
+          name: "Flirting with Disaster",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        },
+        6: {
+          name: "Hard to Kill",
+          descr: "AAAAAAAAAAAAAAAAAAAAAAA"
+        }
+      }
+    }
   }
 };
