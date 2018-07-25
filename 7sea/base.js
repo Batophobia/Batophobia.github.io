52f3f2f03d4abb9290c54a95d01ea9c6d73f2439
Batophobia <godrox3@yahoo.com>
2018-07-24 21:42:12 -0500

    Update base.js

--- 7sea/base.js
+++ 7sea/base.js
@@ -1,3 +1,12 @@
-$(document).on("keyup","#txtSearch",function(e){
-  // timer searching
+$(function() {
+  // Load heroes
+  
+  // Load plot
+  
+  // Add Player
+  
+  // Search
+  $(document).on("keyup","#txtSearch",function(e){
+    // timer searching
+  });
 });
