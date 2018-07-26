edee6b54e59c2181f537631be76b902b01f1c53f
Batophobia <godrox3@yahoo.com>
2018-07-26 16:08:34 -0500

    Update base.js

--- 7sea/base.js
+++ 7sea/base.js
@@ -18,12 +18,28 @@ $(function() {
   // Load plot
   
   // Add Player
+  addHero(gnsp.hero.nico);
   
   // Search
   $(document).on("keyup","#txtSearch",function(e){
     // timer searching
   });
 });
+
+function addHero(objHero){
+  var heroHtml = '<div class="hero ' + objHero.gender.toLowerCase() + '  ' + objHero.nation + '">';
+  heroHtml = heroHtml + '<div class="heroName">'+objHero.name+'</div>';
+  heroHtml = heroHtml + '<<div class="virtHubr"><span class="virtue" descr="'+objHero.virtueDescr+'">'+objHero.virtue+'</span><span class="hubris" descr="'+objHero.hubrisDescr+'">'+objHero.hubris+'</span></div>';
+  heroHtml = heroHtml + '<div class="bgrounds">';
+  for(var bgrnd in objHero.background){
+    heroHtml = heroHtml + '<span class="bground" descr="'+objHero.background[bgrnd]+'">'+bgrnd+'</span>';
+  }
+  heroHtml = heroHtml + '</div>';
+  heroHtml = heroHtml + '<div class="traits">'+objHero.trait.brawn+'</div>';
+  heroHtml = heroHtml + '<div class="skills">'+objHero.skill.athletics+'</div>';
+  heroHtml = heroHtml + '<div class="advantages">'+objHero.advantage[0].name+'</div>';
+  $("#heroes").append("");
+}
 
 function hideAll(){
   $("#heroes").hide();
