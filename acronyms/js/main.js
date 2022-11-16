9febc725598642fc7a248ccd68f8a47ffa5c59df
batophobia <godrox3@yahoo.com>
2022-11-16 10:50:04 -0600

    Initial commit for Acronym cards

---
+++ acronyms/js/main.js
@@ -0,0 +1,50 @@
+var main = {
+	init: function () {
+		settings.init();
+
+		cards.getNext();
+
+		$(".btnSumbit").on('click', function () {
+			const correct = cards.checkAnswer();
+			answerEvent(correct);
+			if (correct) {
+				$("#userInput").val("");
+				cards.getNext();
+			}
+		});
+
+		$(".btnSkip").on('click', function () {
+			$("#userInput").val("");
+			main.alrt(correctString(cards.curr));
+			cards.getNext();
+		});
+
+		$(document).bind('keypress', function (e) {
+			if (e.keyCode == 13) {
+				$('.btnSumbit').trigger('click');
+			}
+		});
+	},
+
+	alrt: function (input) {
+		var alert = $("<div class='alert'>" + input + "</div>");
+		$('#alerts').append(alert);
+		setTimeout(function () {
+			alert.fadeOut('slow', function () {
+				$(this).remove();
+			});
+		}, 3000);
+	}
+};
+
+function answerEvent(correct) {
+	let clr = "#933";
+	if (correct)
+		clr = "#393";
+
+	$("#userInput").css('background-color', clr);
+	setTimeout(() =>
+		$("#userInput").css('background-color', '#333')
+		, 500
+	);
+}
