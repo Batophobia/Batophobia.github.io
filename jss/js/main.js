f408ce948e9154f716556259b0d69c067cd803db
batophobia <godrox3@yahoo.com>
2024-03-14 15:50:45 -0500

    Fix button click trigger

--- jss/js/main.js
+++ jss/js/main.js
@@ -4,24 +4,26 @@ var main = {
   },
   spreadsheet: "1A0j2R5sMb8_EClnbR2Pcm_5_UWHzT50f16yMXjO0MQS",
   sheet: "683326268",
-  key: "TODO",
+  api: "TODO",
+
   load: function () {
-    $(document).on("click", "#btnBegin", main.decrypt);
+    $(document).on("click", "#btnBegin", (e) => { main.decrypt(); });
   },
   decrypt: function () {
     $("#passWrapper").hide();
     $("#siteWrapper").show();
 
     console.log("Decrypting")
-    this.key = $("#pass").val();
+    var key = $("#pass").val();
     this.spreadsheet = vigenere(this.spreadsheet, key);
     this.sheet = vigenere(this.sheet, key);
+    this.api = vigenere(this.api, key);
 
     gapi.load('client', startGAPI);
   },
   startGAPI: function () {
     gapi.client.init({
-      'apiKey': this.key,
+      'apiKey': this.api,
       'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest']
     }).then(function () {
       return gapi.client.sheets.spreadsheets.get({
