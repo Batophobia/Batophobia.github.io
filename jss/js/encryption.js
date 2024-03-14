b659511cfd650b8dc94611f9b7d2f4ac8b1bf4b1
batophobia <godrox3@yahoo.com>
2024-03-14 17:04:18 -0500

    Debugging

--- jss/js/encryption.js
+++ jss/js/encryption.js
@@ -16,3 +16,22 @@ function vigenere(text, key) {
     return String.fromCharCode(((ordA(a) + (26 - ordA(b))) % 26 + 65));
   });
 }
+
+// Encryption function
+async function encrypt(text, key) {
+  const encodedText = new TextEncoder().encode(text);
+  const encodedKey = await crypto.subtle.digest('SHA-256', new TextEncoder().encode(key));
+  const iv = crypto.getRandomValues(new Uint8Array(16));
+  const encrypted = await crypto.subtle.encrypt({ name: 'AES-CBC', iv }, encodedKey, encodedText);
+  return iv.toString() + ':' + Array.from(new Uint8Array(encrypted)).map(b => String.fromCharCode(b)).join('');
+}
+
+// Decryption function
+async function decrypt(encryptedText, key) {
+  const parts = encryptedText.split(':');
+  const iv = new Uint8Array(parts.shift().split(',').map(Number));
+  const ciphertext = new Uint8Array(parts.join(':').split('').map(c => c.charCodeAt(0)));
+  const encodedKey = await crypto.subtle.digest('SHA-256', new TextEncoder().encode(key));
+  const decrypted = await crypto.subtle.decrypt({ name: 'AES-CBC', iv }, encodedKey, ciphertext);
+  return new TextDecoder().decode(decrypted);
+}
