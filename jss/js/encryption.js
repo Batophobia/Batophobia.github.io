06e496e5b627d799303f22e052c5de6746c3c70a
batophobia <godrox3@yahoo.com>
2024-03-21 16:48:55 -0500

    debugging

--- jss/js/encryption.js
+++ jss/js/encryption.js
@@ -1,37 +1,7 @@
-// credit for code - https://rosettacode.org/wiki/Vigen%C3%A8re_cipher#JavaScript
-function ordA(a) {
-  return a.charCodeAt(0) - 65;
-}
-
-// vigenere
-function vigenere(text, key) {
-  if (key == null || key == "") {
-    return text;
+function xor(text, key) {
+  let result = '';
+  for (let i = 0; i < text.length; i++) {
+    result += String.fromCharCode(text.charCodeAt(i) ^ key.charCodeAt(i % key.length));
   }
-
-  var i = 0, b;
-  key = key.toUpperCase().replace(/[^A-Z]/g, '');
-  return text.toUpperCase().replace(/[A-Z]/g, function (a) {
-    b = key[i++ % key.length];
-    return String.fromCharCode(((ordA(a) + (26 - ordA(b))) % 26 + 65));
-  });
-}
-
-// Encryption function
-async function encrypt(text, key) {
-  const encodedText = new TextEncoder().encode(text);
-  const encodedKey = await crypto.subtle.digest('SHA-256', new TextEncoder().encode(key));
-  const iv = crypto.getRandomValues(new Uint8Array(16));
-  const encrypted = await crypto.subtle.encrypt({ name: 'AES-CBC', iv }, encodedKey, encodedText);
-  return iv.toString() + ':' + Array.from(new Uint8Array(encrypted)).map(b => String.fromCharCode(b)).join('');
-}
-
-// Decryption function
-async function decrypt(encryptedText, key) {
-  const parts = encryptedText.split(':');
-  const iv = new Uint8Array(parts.shift().split(',').map(Number));
-  const ciphertext = new Uint8Array(parts.join(':').split('').map(c => c.charCodeAt(0)));
-  const encodedKey = await crypto.subtle.digest('SHA-256', new TextEncoder().encode(key));
-  const decrypted = await crypto.subtle.decrypt({ name: 'AES-CBC', iv }, encodedKey, ciphertext);
-  return new TextDecoder().decode(decrypted);
+  return result;
 }
--- jss/js/main.js
+++ jss/js/main.js
@@ -2,14 +2,14 @@ var main = {
   init: function () {
     this.load();
   },
-  spreadsheet: "1A0j2R5sMb8_EClnbR2Pcm_5_UWHzT50f16yMXjO0MQS",
+  spreadsheet: "B=Q\u0003Y7LQ0\u000cY-,.\n]\u0018<S>\u000e\u0010&\u0006,66#\u001b5L\u0003\u001bEW\u0015?=\nvC=23",
   sheet: "683326268",
-  clientID: "160556897237-k3l8icc4oc7fcbf04yx0h85tgjrwt682",
-  clientSecret: "YHCJZB-A-p8LCefXFvnwtOBK7-hkpZPbvU9",
-  api: "SBzrCcZ0xcwv-3me2rMoUaZzjPkmTW2C6Ag7NRV",
+  clientID: "BBQG^SA\nDFREF\u0016J@K\u001d\r\u0001_\u000e\u001c\u0004\u001d\u001e\u0003\u001d[Q\u0016GC\u001eYG\t\u000b\u0013R\u001e\u0004WJY",
+  clientSecret: '4;"!;=Tp^\u000cY!(\u000b\u000fg;\u0010\u0014\u0005\u0008 !~DY\u0011\u0000\u001b,?K\u000b7X',
+  api: "2=\u001b\u00138\u001c;\u0003\u0015\u001e\u0016\u0017FV\u001aRA\u00004\u0004>\u000f)E\u001f,\u0013\u001f((KjE7\u000eE>7<",
 
   load: function () {
-    console.log("ver .001");
+    console.log("ver .003");
 
     $("#btnBegin").on("click", main.decrypt);
   },
@@ -18,27 +18,31 @@ var main = {
     $("#siteWrapper").show();
 
     var key = $("#pass").val();
-    main.clientID = vigenere(main.clientID, key);
+    main.clientID = xor(main.clientID, key);
     main.clientID += ".apps.googleusercontent.com";
-    main.clientSecret = vigenere(main.clientSecret, key);
-    main.api = vigenere(main.api, key);
-    main.spreadsheet = vigenere(main.spreadsheet, key);
+    main.clientSecret = xor(main.clientSecret, key);
+    main.api = xor(main.api, key);
+    main.spreadsheet = xor(main.spreadsheet, key);
 
     gapi.load('client', main.startGAPI);
   },
-  startGAPI: function () {
-    console.log({ api: main.api, ss: main.spreadsheet, id: main.clientID, cs: main.clientSecret })
-    gapi.client.init({
-      'apiKey': main.api,
-      'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest']
-    }).then(function () {
-      return gapi.client.sheets.spreadsheets.get({
+  startGAPI: async function () {
+    try {
+      console.log("test00")
+      await gapi.client.init({
+        'apiKey': main.api,
+        'discoveryDocs': ['https://sheets.googleapis.com/$discovery/rest']
+      });
+
+      console.log("test01")
+      const response = gapi.client.sheets.spreadsheets.get({
         spreadsheetId: main.spreadsheet,
       });
-    }).then(function (response) {
       console.log(response.result);
-    }, function (reason) {
-      console.log({ reason });
-    });
+
+    } catch (error) {
+      console.log("test04");
+      console.log({ error });
+    };
   }
 };
