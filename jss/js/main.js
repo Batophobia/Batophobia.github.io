3137f5ea44c8331762f33a4e14f2133a8dd596fa
batophobia <godrox3@yahoo.com>
2024-03-22 15:46:13 -0500

    debugging

--- jss/js/main.js
+++ jss/js/main.js
@@ -3,7 +3,7 @@ var main = {
     this.load();
   },
   load: function () {
-    console.log("ver .009");
+    console.log("ver .01");
 
     $("#btnBegin").on("click", goog.decrypt);
     $("#btnSignIn").on("click", goog.signIn);
