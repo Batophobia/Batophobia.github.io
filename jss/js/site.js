9755f74812c03e26dc5ecb836ff3a9061951f38f
batophobia <godrox3@yahoo.com>
2024-05-17 17:14:26 -0500

    Import debug

--- jss/js/site.js
+++ jss/js/site.js
@@ -21,6 +21,32 @@ var site = {
 
   convertRows: function () {
     orderData = $("#importOrder").val()
+    if (orderData == "") return;
+
+    orderData = orderData.substring(orderData.indexOf("Order #"), orderData.indexOf("Delivery method")).split('\n')
     console.log(orderData)
+    let idx = 0;
+
+    const orderNum = orderData[0].split("#")[1];
+
+    while (!/Items \(\d+\)/.test(orderData[idx]))
+      idx++;
+
+    const numItems = orderData[idx].split("(")[1].split(")")[0];
+
+    idx += 2;
+
+    while (orderData[idx] != "Payment info") {
+      let orderEnd = idx;
+      for (; /\$\d+.\d\d/.test(orderData[orderEnd]); orderEnd++) { }
+      orderEnd++;
+
+      console.log(orderData.filter((v, i) => i >= idx && i <= orderEnd))
+      /*
+      */
+      idx = orderEnd + 1;
+    }
+
+    console.log({ orderNum, numItems, idx })
   },
 };
