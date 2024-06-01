8741ee952ad4b3659de8e4041a777863c8ed06d2
batophobia <godrox3@yahoo.com>
2024-05-31 22:00:12 -0500

    Re auth on error

--- jss/js/main.js
+++ jss/js/main.js
@@ -6,7 +6,7 @@ var main = {
     this.load();
   },
   load: function () {
-    console.log("ver 1.001");
+    console.log("ver 1.1");
 
     $("#btnBegin").on("click", goog.decrypt);
     $("#pass").focus();
