301035f575a8e771fb1b47e499105a1914ec157c
batophobia <godrox3@yahoo.com>
2025-01-09 17:39:55 -0600

    Tab tool

--- tools/tab-name.js
+++ tools/tab-name.js
@@ -1,27 +1,15 @@
 document.head = document.head || document.getElementsByTagName('head')[0];
+const faviconElem = document.getElementById('favicon');
 
 function updateTab() {
-  titleElem = document.getElementById("tabTitle");
-  faviconElem = document.getElementById("tabFavicon");
-  document.title = titleElem.value;
+  let titleInput = document.getElementById("tabTitle");
+  let faviconInput = document.getElementById("tabFavicon");
+  document.title = titleInput.value;
 
-  // var link = document.querySelector("link[rel~='icon']");
-  // if (!link) {
-  //   link = document.createElement('link');
-  //   link.rel = 'icon';
-  //   document.head.appendChild(link);
-  // }
-  // link.href = 'https://stackoverflow.com/favicon.ico';
-}
-
-function changeFavicon(src) {
-  var link = document.createElement('link'),
-    oldLink = document.getElementById('dynamic-favicon');
-  link.id = 'dynamic-favicon';
-  link.rel = 'shortcut icon';
-  link.href = src;
-  if (oldLink) {
-    document.head.removeChild(oldLink);
-  }
-  document.head.appendChild(link);
+  const reader = new FileReader();
+  reader.onload = function (e) {
+    const faviconLink = document.getElementById('favicon');
+    faviconLink.href = e.target.result;
+  };
+  reader.readAsDataURL(faviconInput.files[0]);
 }
