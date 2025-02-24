7fc12d8afe7ffe3bb5b726d59672cca9c13af51a
Batophobia <godrox3@yahoo.com>
2025-02-23 19:37:03 -0600

    Fix version

--- Bible/js/main.js
+++ Bible/js/main.js
@@ -9,7 +9,7 @@ function onSignIn(user) {
 }
 
 function getBibles(){
-  $.get(`https://bible-api.com/Genesis+1:1?translation=nasb`)
+  $.get(`https://bible-api.com/Genesis+1:1?translation=asv`)
   .done(function(data){
     $("#passageText").html(data);
   });
