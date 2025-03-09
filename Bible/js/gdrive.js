edbf467ebced18ff26ca98b69698a56ffc5c0149
batophobia <godrox3@yahoo.com>
2025-03-08 22:39:11 -0600

    Update scope

--- Bible/js/gdrive.js
+++ Bible/js/gdrive.js
@@ -7,7 +7,7 @@ var DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"
 
 // Authorization scopes required by the API; multiple scopes can be
 // included, separated by spaces.
-var SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly";
+var SCOPES = "https://www.googleapis.com/auth/spreadsheets";
 
 var authorizeButton;
 var signoutButton;
